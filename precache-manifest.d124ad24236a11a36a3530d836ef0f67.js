99b7547074a82ae2cd5959e471b8dbc830e021f7
Ankur Gupta <ankgupta32@publicisgroupe.net>
2020-05-13 15:15:41 +0530

    Updates

---
+++ precache-manifest.d124ad24236a11a36a3530d836ef0f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8fab108e84b3ec464da7b73ed01ff5",
+    "url": "/web-share/index.html"
+  },
+  {
+    "revision": "2a0cbc1624989af9e1c1",
+    "url": "/web-share/static/css/main.7bca7d5a.chunk.css"
+  },
+  {
+    "revision": "d9e25341beac6a8d62b1",
+    "url": "/web-share/static/js/2.c7ad19a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-share/static/js/2.c7ad19a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0cbc1624989af9e1c1",
+    "url": "/web-share/static/js/main.57258c1d.chunk.js"
+  },
+  {
+    "revision": "5ea560cb4d59b8ef9909",
+    "url": "/web-share/static/js/runtime-main.e60dc79e.js"
+  }
+]);
